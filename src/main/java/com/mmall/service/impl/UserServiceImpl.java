e800b5471f6b5cbba32fd24abd00d71582cbe732
liuf2 <fee9702@gmail.com>
2019-01-01 16:30:54 +0800

    add category

--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -173,4 +173,13 @@ public class UserServiceImpl implements IUserService {
             user.setPassword(StringUtils.EMPTY);
             return ServerResponse.createBySuccess(user);
         }
+
+
+        //backend
+        public ServerResponse checkAdminRole(User user){
+        if (user != null && user.getRole().intValue() == Const.Role.ROLE_ADMIN){
+            return ServerResponse.createBySuccess();
+        }
+            return ServerResponse.createByError();
+    }
 }
